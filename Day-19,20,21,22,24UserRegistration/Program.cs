c01c361b6a6d39c86618c44a8791920adf945bb2
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-06 17:40:09 +0530

    Add project files.

---
+++ Day-19,20,21,22,24UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistrationUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationValidation obj = new UserRegistrationValidation();
+
+            Console.WriteLine("Checking Validation of User Input : ");
+
+            obj.validateFirstName();
+
+        }
+    }
+}
---
+++ Day-19,20,21,22,24UserRegistration/UserRegistrationValidation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    class UserRegistrationValidation
+    {
+
+        public void validateFirstName()
+        {
+            string[] Regex_Array = new string[6];
+
+            //first name
+            Regex_Array[0] = "^[A-Z][A-Za-z]{2,}$";
+            //last name
+            Regex_Array[1] = "^[A-Z][A-Za-z]{2,}$";
+            //email id
+            Regex_Array[2] = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+            // Phone no
+            Regex_Array[3] = "^[1-9]{2}[ ][0-9]{10}$";
+            //password
+            Regex_Array[4] = "^([a-zA-Z0-9])*[!@#$%^&*]{1}([a-zA-Z0-9])*$";
+            //sample email id
+            Regex_Array[5] = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";  
+
+            string[] User_Input_Array = new string[6];
+
+            Console.WriteLine("1] Enter First Name :");
+            User_Input_Array[0] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("2] Enter Last Name :");
+            User_Input_Array[1] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("3] Enter the Email Id : ");
+            User_Input_Array[2] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("4] Enter the Phone No : ");
+            User_Input_Array[3] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("5] Enter the Password : ");
+            User_Input_Array[4] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("6] Enter the Sample Email Id : ");
+            User_Input_Array[5] = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Entered input is Valid or Not T/F : ");
+            Console.WriteLine();
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                Func<string, bool> nameValidater = User_Input_Array => Regex.IsMatch(User_Input_Array, Regex_Array[i]);
+                bool result = nameValidater(User_Input_Array[i]);
+                Console.WriteLine((i + 1) + "] " + result);
+
+            }
+        }
+    }
+}
+
